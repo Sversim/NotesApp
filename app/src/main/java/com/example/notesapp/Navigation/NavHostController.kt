86adfa85ec3b29144700893cf110f544b00ad893
Sversim <Sversim@gmail.com>
2023-01-09 13:19:48 +1000

    Navigations
    Дополнение навигации. Добавление функций экранов

--- app/src/main/java/com/example/notesapp/Navigation/NavHostController.kt
+++ app/src/main/java/com/example/notesapp/Navigation/NavHostController.kt
@@ -1,6 +1,10 @@
 package com.example.notesapp.Navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.notesapp.MainViewModel
 
 sealed class NavRoute(val route: String) {
     object StartScreen: NavRoute("start_screen");
@@ -8,5 +12,19 @@ sealed class NavRoute(val route: String) {
 }
 
 @Composable
-fun NavHostController() {
+fun NavHostController(mViewModel: MainViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavRoute.StartScreen.route) {
+        composable(NavRoute.StartScreen.route) {StartScreen(navHostController = navController, viewModel = mViewModel)}
+
+        composable(NavRoute.NoteScreen.route + "/{Id}") {
+                backStackEntry ->
+            NoteScreen(
+                navHostController = navController,
+                viewModel = mViewModel,
+                noteId = backStackEntry.arguments?.getString("Id")
+            )
+        }
+    }
 }
